04d65849d8863d0a6b1e01a1ecf6ad58d7f0eefc
ArthurVenturi <artoriusvt24@gmail.com>
2025-05-14 20:43:25 -0300

    Ajustes no design na página de cadastro, login, enviar e o cabeçalho

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -12,11 +12,17 @@ const Cadastro = () => {
 
     return (
         <div className="cadastro-container">
-            <h2>Criar uma Conta</h2>
-            <input type="text" placeholder="Nome" value={username} onChange={(e) => setUsername(e.target.value)} minLength={8} maxLength={100} pattern='' required />
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} minLength={8} maxLength={100} pattern='' required />
-            <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} minLength={8} maxLength={100} pattern='' required />
-            <button onClick={() => handleRegister()} style={{ borderRadius: '20px' }}>Criar Conta</button>
+            <h1>Cadastro</h1>
+            <i className='pi pi-user'>
+                <input type="text" placeholder="Nome" value={username} onChange={(e) => setUsername(e.target.value)} minLength={8} maxLength={100} pattern='' required />
+            </i>
+            <i className='pi pi-envelope'>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} minLength={8} maxLength={100} pattern='^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$' onInvalid={e => e.target.setCustomValidity("Deve ser um email válido")} required />
+            </i>
+            <i className='pi pi-lock'>
+                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} minLength={8} maxLength={100} pattern='' onInvalid={e => e.target.setCustomValidity("A senha deve conter 8 caracteres")} required />
+            </i>
+            <button onClick={() => handleRegister()}>Criar Conta</button>
         </div>
     );
 }
